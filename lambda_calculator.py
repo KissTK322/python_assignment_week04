32ccad566a7c5cdd898b849c6d000e675cf84c96
Natthawat phummarin <np@MacBook-Air-khxng-Natthawat-2.local>
2025-07-22 10:47:18 +0700

    Initial commit with lambda_calculator.py

---
+++ lambda_calculator.py
@@ -0,0 +1,9 @@
+add = lambda num1 , num2 : num1 + num2
+subtract = lambda num1 , num2 : num1 - num2
+multiply = lambda num1 , num2 : num1 * num2
+divide = lambda num1 , num2 : num1 / num2
+
+print(add(1 , 3))
+print(add(45 , 22))
+print(add(2 , 9))
+print(add(8 , 2))
