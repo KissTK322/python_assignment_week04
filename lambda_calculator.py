93c058d934743cb20dc33d1ab5b8e0ac682652db
Natthawat phummarin <np@MacBook-Air-khxng-Natthawat-2.local>
2025-07-22 11:15:52 +0700

    Create main2.py

--- lambda_calculator.py
+++ lambda_calculator.py
@@ -3,7 +3,12 @@ subtract = lambda num1 , num2 : num1 - num2
 multiply = lambda num1 , num2 : num1 * num2
 divide = lambda num1 , num2 : num1 / num2
 
-print(add(1 , 3))
-print(add(45 , 22))
-print(add(2 , 9))
-print(add(8 , 2))
+def main():
+    print(add(1 , 3))
+    print(subtract(45 , 22))
+    print(multiply(2 , 9))
+    print(divide(8 , 2))
+
+
+if __name__ == '__main__':
+    main()
