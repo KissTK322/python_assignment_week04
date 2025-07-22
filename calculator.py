7f373571ee7e94cda8b4529a0cd2aafd620d5019
Natthawat phummarin <np@MacBook-Air-khxng-Natthawat-2.local>
2025-07-22 10:39:09 +0700

    Initial commit with calculator.py

---
+++ calculator.py
@@ -0,0 +1,16 @@
+def add(num1 , num2) :
+    return num1 + num2
+
+def subtract(num1 , num2) :
+    return num1 - num2
+
+def multiply(num1 , num2) :
+    return num1 * num2
+
+def divide(num1 , num2) :
+    return num1 / num2
+
+print(add(1 , 3))
+print(add(45 , 22))
+print(add(2 , 9))
+print(add(8 , 2))
