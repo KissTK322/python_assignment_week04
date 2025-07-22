e555247275a8a60eca17022766bd82150d826ae0
Natthawat phummarin <np@MacBook-Air-khxng-Natthawat-2.local>
2025-07-22 11:14:20 +0700

    Create main2.py

--- calculator.py
+++ calculator.py
@@ -10,7 +10,13 @@ def multiply(num1 , num2) :
 def divide(num1 , num2) :
     return num1 / num2
 
-print(add(1 , 3))
-print(add(45 , 22))
-print(add(2 , 9))
-print(add(8 , 2))
+
+def main():
+    print(add(1 , 3))
+    print(add(45 , 22))
+    print(add(2 , 9))
+    print(add(8 , 2))
+
+
+if __name__ == '__main__':
+    main()
