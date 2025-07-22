25afa393d14b07aee0b57d9058d178c6604c6be3
Natthawat phummarin <np@MacBook-Air-khxng-Natthawat-2.local>
2025-07-22 11:07:38 +0700

    Create Utilities

---
+++ utilities/string_operations.py
@@ -0,0 +1,11 @@
+def reverse_string (str) :
+    return str[::-1]
+
+def capitalize_string(str) :
+    return str[0].capitalize() + str[1 : ]
+
+def lowercase_string(str) :
+    return str.lower()
+
+def uppercase_string(str) :
+    return str.upper()
