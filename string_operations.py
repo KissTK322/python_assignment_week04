3486a0f650e589cb0c693e5e1a188b4cd95e8353
Natthawat phummarin <np@MacBook-Air-khxng-Natthawat-2.local>
2025-07-22 11:01:40 +0700

    Change string_operations

--- string_operations.py
+++ string_operations.py
@@ -2,7 +2,7 @@ def reverse_string (str) :
     return str[::-1]
 
 def capitalize_string(str) :
-    return str[0].capitalize()
+    return str[0].capitalize() + str[1 : ]
 
 def lowercase_string(str) :
     return str.lower()
