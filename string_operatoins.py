a24be509084c62a5520912bd7cc5dde17a9e7d5c
Natthawat phummarin <np@MacBook-Air-khxng-Natthawat-2.local>
2025-07-22 10:58:54 +0700

    Change string_operations

--- string_operatoins.py
+++ string_operatoins.py
@@ -1,4 +1,11 @@
 def reverse_string (str) :
     return str[::-1]
 
-print(reverse_string("Hello"))
+def capitalize_string(str) :
+    return str[0].capitalize()
+
+def lowercase_string(str) :
+    return str.lower()
+
+def uppercase_string(str) :
+    return str.upper()
